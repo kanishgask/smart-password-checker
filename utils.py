ea71f3ae494f94037366b5a0e0f8d6134df37c25
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-03-31 20:10:46 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+def password_tips():
+    
+    tips = [
+        "Use at least 8 characters",
+        "Include uppercase letters",
+        "Include numbers",
+        "Use special characters",
+        "Avoid common words"
+    ]
+    
+    return tips
